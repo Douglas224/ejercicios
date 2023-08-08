6e5fa585dc2a839b17bba7bc9ca5842fa603d2c4
Douglas224 <139875425+Douglas224@users.noreply.github.com>
2023-08-08 09:02:04 -0500

    Add files via upload

---
+++ ejercicios.js/1.ejercicio.js/app.js
@@ -0,0 +1,20 @@
+
+'use strict';
+/*Cristian Andres Vega
+*Douglas Alexis Fajardo Valdes
+*1 Realizar un programa que pida al usuario 2 números y se realicen las 4 operaciones básicas (Suma, resta, multiplicación, división). Imprimir los resultados.
+*/
+let num1=parseInt(prompt(" digite un numero"));
+    
+let num2=parseInt(prompt("digite un segundo numero"));
+    
+    let suma=num1+num2;
+    alert("la suma es igual a "+suma);
+    let resta=num1-num2;
+    alert("la resta es igual a "+resta);
+    let multiplicacion=num1*num2;
+    alert("la multiplicacion es"+multiplicacion);
+    let division=num1/num2;
+    alert("la division es"+division);
+
+
---
+++ ejercicios.js/2.ejercicio.js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+/*Cristian Andres Vega
+*Douglas Alexis Fajardo Valdes
+*2. Leer un número e imprimir un mensaje si es positivo o negativo.
+*/
+let numero = (prompt("Ingrese un número: "));
+if (numero >= 0) {
+    alert("El número es positivo.");
+} else {
+    alert("El número es negativo.");
+}
